df4d2ba53f35d63cde7185e3ae6e76f97928b1d1
Jeremy Magland <jmagland@flatironinstitute.org>
2026-03-31 15:33:24 -0400

    Fix compile.m path resolution

--- compile.m
+++ compile.m
@@ -1,7 +1,7 @@
 % Compile MEX files for hello_mip_mex
 fprintf('Compiling hello_mip_mex MEX files...\n');
 
-src_dir = fullfile(fileparts(mfilename('fullpath')), 'hello_mip_mex');
+src_dir = fileparts(mfilename('fullpath'));
 original_dir = pwd;
 cd(src_dir);
 
